8c84682cf456c8df45a15304704c5403895c67ad
Blackwatch98 <57369441+Blackwatch98@users.noreply.github.com>
2021-04-14 23:50:52 +0200

    knight movement

--- chessvr/Assets/Scripts/Pieces/Knight.cs
+++ chessvr/Assets/Scripts/Pieces/Knight.cs
@@ -4,10 +4,30 @@ using UnityEngine;
 
 public class Knight : Piece
 {
+    Vector2Int[] offsets = new Vector2Int[]
+    {
+        new Vector2Int(2, 1),
+        new Vector2Int(2, -1),
+        new Vector2Int(1, 2),
+        new Vector2Int(1, -2),
+        new Vector2Int(-2, 1),
+        new Vector2Int(-2, -1),
+        new Vector2Int(-2, 1),
+        new Vector2Int(-1, -2),
+    };
     public override List<Vector2Int> SelectAvailableSquares()
     {
         availableMoves.Clear();
-        availableMoves.Add(occupiedSquare + new Vector2Int(0, 1));
+
+        for (int i = 1; i < offsets.Length; i++)
+        {
+            Vector2Int nextCoordinates = occupiedSquare + offsets[i];
+            Piece piece = board.GetPieceOnSquare(nextCoordinates);
+            if (!board.CheckIfCoordinatesAreOnBoard(nextCoordinates))
+                continue;
+            if (piece == null || (!piece.IsFromSameTeam(this))
+                TryToAddMove(nextCoordinates);
+        }
         return availableMoves;
     }
 
