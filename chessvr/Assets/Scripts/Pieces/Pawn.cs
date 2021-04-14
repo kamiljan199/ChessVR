a2f0b580b553abebc5c2f04226d52e91565db03d
Blackwatch98 <57369441+Blackwatch98@users.noreply.github.com>
2021-04-15 00:20:07 +0200

    queen movement

--- chessvr/Assets/Scripts/Pieces/Pawn.cs
+++ chessvr/Assets/Scripts/Pieces/Pawn.cs
@@ -6,7 +6,11 @@ public class Pawn : Piece
     public override List<Vector2Int> SelectAvailableSquares()
     {
         availableMoves.Clear();
+
+        //black pawns can move ony down and white only up
         Vector2Int direction = team == TeamColor.White ? Vector2Int.up : Vector2Int.down;
+        
+        //its 2 - when has not moved yet, 1 - when its not its first move
         float range = hasMoved ? 1 : 2;
         for(int i = 1; i <= range; i++)
         {
